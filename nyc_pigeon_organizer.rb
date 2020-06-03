bf05498ea4fc3ac274efd1a1cd4a6d560ffade36
interactive-directory-1839 <benthedunn@gmail.com>
2020-06-03 20:27:03 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  
+  data.each do |attributes, value|
+    value.each do |info, names|
+      names.each do |name|
+        if new_hash[name] == nil
+          new_hash[name] = {}
+        end
+        if new_hash[name][attributes] == nil
+          new_hash[name][attributes] = []
+        end
+        new_hash[name][attributes].push(info.to_s)
+      end
+    end
+  end
+  new_hash
 end
