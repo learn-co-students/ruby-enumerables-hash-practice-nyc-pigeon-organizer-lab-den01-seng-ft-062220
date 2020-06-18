b80e018cff28768c6cbe0dc4c7cd2612cb2a8971
Bradley Calkins <b.raders1013@gmail.com>
2020-06-18 04:32:26 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 def nyc_pigeon_organizer(data)
   hash = {}
   final_results = data.each_with_object(hash) do |(key, value), final_array|
-    puts(value, final_array)
+   # puts(value, final_array)
     value.each do |inner_key, names|
       #puts(inner_key, names)
       names.each do | name|
